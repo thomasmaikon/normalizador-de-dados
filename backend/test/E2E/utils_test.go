1088298d0b419470395375c83c1c6c79d7e8c3a8
thomas <thomasmaikon@hotmail.com>
2023-05-05 18:21:37 -0300

    Feat-Test: Implementando setup para criacao de tests e2e

---
+++ backend/test/E2E/utils_test.go
@@ -0,0 +1,91 @@
+package E2E
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+)
+
+type DockerInfo struct {
+	User        string
+	Password    string
+	Dbname      string
+	HostAndPort string
+	Resource    *dockertest.Resource
+	Pool        *dockertest.Pool
+}
+
+func StartPostgresDockerFormTest() *DockerInfo {
+	err := godotenv.Load("../../config.env")
+	if err != nil {
+		log.Fatal("Env File doesnot Find")
+	}
+
+	user := os.Getenv("user")
+	password := os.Getenv("password")
+	dbname := os.Getenv("dbname")
+
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not construct pool: %s", err)
+	}
+
+	err = pool.Client.Ping()
+	if err != nil {
+		log.Fatalf("Could not connect to Docker: %s", err)
+	}
+
+	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "11",
+		Env: []string{
+			"POSTGRES_PASSWORD=" + password,
+			"POSTGRES_USER=" + user,
+			"POSTGRES_DB=" + dbname,
+			"listen_addresses = '*'",
+		},
+	}, func(config *docker.HostConfig) {
+		// set AutoRemove to true so that stopped container goes away by itself
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{Name: "no"}
+	})
+
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	hostAndPort := resource.GetHostPort("5432/tcp")
+	databaseUrl := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", user, password, hostAndPort, dbname)
+
+	log.Println("Connecting to database on url: ", databaseUrl)
+
+	resource.Expire(60) // Tell docker to hard kill the container in 120 second
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	pool.MaxWait = 120 * time.Second
+
+	var db *sql.DB
+
+	if err = pool.Retry(func() error {
+		db, err = sql.Open("postgres", databaseUrl)
+		if err != nil {
+			return err
+		}
+		return db.Ping()
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+	return &DockerInfo{
+		User:        user,
+		Password:    password,
+		Dbname:      dbname,
+		HostAndPort: hostAndPort,
+		Resource:    resource,
+		Pool:        pool,
+	}
+}
