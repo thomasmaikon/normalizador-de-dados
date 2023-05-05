be690494a52a210452ad2cb69cd1744f187337b4
thomas <thomasmaikon@hotmail.com>
2023-05-05 15:27:06 -0300

    Fix: chave estrangeira na entidade user

--- backend/models/login.go
+++ backend/models/login.go
@@ -2,6 +2,6 @@ package models
 
 type Login struct {
 	ID       int    `gorm:"primaryKey;autoIncrement"`
-	Email    string `gorm:"not null;unique"`
+	Email    string `gorm:"unique;not null"`
 	Password string `gorm:"not null"`
 }
