b0605d8b8fb2e857993c1c4f2737bf47ee419f5b
thomas <thomasmaikon@hotmail.com>
2023-05-05 10:07:57 -0300

    Implementando conexao com banco de dados utilizando pool de conexoes

--- backend/main.go
+++ backend/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"hubla/desafiofullstack/setup"
+	"hubla/desafiofullstack/utils"
 )
 
 func main() {
+	db := utils.GetDB()
+	defer db.DB()
+
 	setup.NewAppEngine().
 		InitializeRoutes().
 		RunMigrations().
---
+++ backend/utils/Database.go
@@ -0,0 +1,77 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var dbConnection *gorm.DB
+
+func GetDB() *gorm.DB {
+
+	if dbConnection == nil {
+		host, port, user, password, dbname := readEnvironments("config.env")
+
+		dbConnection = openDatabaseConnection(host, port, user, password, dbname)
+	}
+
+	return dbConnection
+}
+
+func GetDBWithParams(port string, user, password, dbname string) *gorm.DB {
+
+	if dbConnection == nil {
+
+		dbConnection = openDatabaseConnection("localhost",
+			port,
+			user,
+			password,
+			dbname)
+	}
+
+	return dbConnection
+}
+
+func readEnvironments(file string) (string, string, string, string, string) {
+
+	err := godotenv.Load("config.env")
+	if err != nil {
+		log.Panic("Env File doesnot Find")
+	}
+
+	host := os.Getenv("host")
+	port := os.Getenv("port")
+	user := os.Getenv("user")
+	password := os.Getenv("password")
+	dbname := os.Getenv("dbname")
+
+	return host, port, user, password, dbname
+}
+
+func openDatabaseConnection(host, port, user, password, dbname string) *gorm.DB {
+	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := gorm.Open(postgres.Open(psqlconn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	sql, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+
+	sql.SetMaxOpenConns(5)
+	sql.SetConnMaxIdleTime(5)
+	sql.SetConnMaxLifetime(time.Hour)
+
+	return db
+}
