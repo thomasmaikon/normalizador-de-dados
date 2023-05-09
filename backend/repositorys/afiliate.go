e4a23213b80e932cee908295d950bdbeb4994d67
thomas <thomasmaikon@hotmail.com>
2023-05-09 11:27:38 -0300

    Adaptando produto e afiliado para utilizar o unitof work

--- backend/repositorys/afiliate.go
+++ backend/repositorys/afiliate.go
@@ -3,33 +3,40 @@ package repositorys
 import (
 	"database/sql"
 	"hubla/desafiofullstack/dtos"
+	"hubla/desafiofullstack/entitys"
 	"hubla/desafiofullstack/querys"
 	"hubla/desafiofullstack/utils"
-
-	"gorm.io/gorm"
 )
 
 type IAfliateRepository interface {
 	AddNewAfiliate(inputAfiliate *dtos.AfiliatedDTO, email string, idCreator int) (bool, error)
+	Find(name string, creatorId int) (*entitys.Afiliated, error)
 }
 
 type afiliateRepository struct {
-	db *gorm.DB
+	uow *utils.UnitOfWork
 }
 
 func NewAfiliateRepository() IAfliateRepository {
 	return &afiliateRepository{
-		db: utils.GetDB(),
+		uow: utils.GetUnitOfWork(),
 	}
 }
 
 func (repository *afiliateRepository) AddNewAfiliate(inputAfiliate *dtos.AfiliatedDTO, email string, idCreator int) (bool, error) {
 
-	result := repository.db.Exec(querys.AddAfiliate,
+	result := repository.uow.GetDB().Exec(querys.AddAfiliate,
 		sql.Named(querys.NamedName, inputAfiliate.Name),
 		sql.Named(querys.NamedCreatorsId, idCreator),
 		sql.Named(querys.NamedEmail, email),
 	)
 
 	return result.RowsAffected > 0, result.Error
+}
+
+func (repository *afiliateRepository) Find(name string, creatorId int) (*entitys.Afiliated, error) {
+	var afialte entitys.Afiliated
+	err := repository.uow.GetDB().Table("AFILIATEDS").Select("*").Where("name = '?' AND creator_id = ?", name, creatorId).Scan(&afialte)
+
+	return &afialte, err.Error
 }
--- backend/repositorys/product.go
+++ backend/repositorys/product.go
@@ -3,35 +3,48 @@ package repositorys
 import (
 	"database/sql"
 	"hubla/desafiofullstack/dtos"
+	"hubla/desafiofullstack/entitys"
 	"hubla/desafiofullstack/querys"
 	"hubla/desafiofullstack/utils"
-
-	"gorm.io/gorm"
 )
 
 type IProductRepository interface {
 	CreateNewProduct(newProduct *dtos.ProductDTO, email string, idCreator int) (bool, error)
+	Find(description string, creatorId int) (*entitys.Product, error)
 }
 
 type productRepository struct {
-	db *gorm.DB
+	uow *utils.UnitOfWork
 }
 
 func NewProductRepository() IProductRepository {
 	return &productRepository{
-		db: utils.GetDB(),
+		uow: utils.GetUnitOfWork(),
 	}
 }
 
 func (repository *productRepository) CreateNewProduct(newProduct *dtos.ProductDTO, email string, idCreator int) (bool, error) {
 
-	err := repository.db.Exec(
+	err := repository.uow.GetDB().Exec(
 		querys.CreateNewProduct,
 		sql.Named(querys.NamedDescription, newProduct.Description),
 		sql.Named(querys.NamedCreatorsId, idCreator),
 		sql.Named(querys.NamedEmail, email),
 		sql.Named(querys.NamedPrice, newProduct.Price),
 	)
-	
+
 	return err.RowsAffected > 0, err.Error
+}
+
+func (repository *productRepository) Find(description string, creatorId int) (*entitys.Product, error) {
+
+	var product entitys.Product
+
+	err := repository.uow.GetDB().
+		Table("PRODUCTS").
+		Select("*").
+		Where("id_creator = ? AND description = '?'", creatorId, description).
+		Scan(&product)
+
+	return &product, err.Error
 }
