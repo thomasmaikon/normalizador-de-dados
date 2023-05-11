6d093ba921193dd71c90cc425954ec280000a38a
thomas <thomasmaikon@hotmail.com>
2023-05-11 15:53:33 -0300

    Removendo variaveis nao utilizadas

--- backend/entitys/creator.go
+++ backend/entitys/creator.go
@@ -3,7 +3,6 @@ package entitys
 type Creator struct {
 	ID       int `gorm:"autoIncrement;primaryKey"`
 	Name     string
-	LeftOver uint64
 	UserID   int
 	User     User
 }
--- backend/models/creator.go
+++ backend/models/creator.go
@@ -3,5 +3,4 @@ package models
 type CreatorModel struct {
 	CreatorId int
 	Name      string
-	LeftOver  uint64
 }
